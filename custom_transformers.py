fixed bug in zero variance transformer

custom_transformers.py
@@ -73,19 +73,23 @@ class ZeroVariance(BaseEstimator, TransformerMixin):
         n_obs = X.shape[0]
 
         for i, col in enumerate(X.T):
+            # obtain values, counts of values and sort counts from
+            # most to least frequent
             val_counts = np.unique(col, return_counts= True)
-            sort_val_counts = np.sort(val_counts, axis = 0)
+            counts = val_counts[1]
+            counts_len = counts.shape[0]
+            counts_sort = np.sort(counts)[::-1]
 
-            if len(sort_val_counts[0]) == 1:
-                #unique_pct = (len(sort_val_counts[0]) / n_obs) * 100
+            # if only one value, is ZV
+            if counts_len == 1:
                 self.zero_var[i] = True
                 self.near_zero_var[i] = True
                 continue
 
             # ratio of most frequent / second most frequent
-            freq_ratio = sort_val_counts[1][0] / sort_val_counts[1][1]
+            freq_ratio = counts_sort[0] / counts_sort[1]
             # percent unique values
-            unique_pct = (len(sort_val_counts[0]) / n_obs) * 100
+            unique_pct = (counts_len / n_obs) * 100
 
             if (unique_pct < self.unique_cut) and (freq_ratio > self.freq_cut):
                  self.near_zero_var[i] = True